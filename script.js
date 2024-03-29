cc55c4e764c5585679a4dbe8e8153a08e6967a8c
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2024-03-30 00:32:01 +0500

    Add more button variables to script

--- script.js
+++ script.js
@@ -6,4 +6,6 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
-let button1 = document.querySelector("#button1");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
