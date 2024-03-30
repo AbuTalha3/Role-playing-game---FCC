e8eacd05b8371077e4ebcf2b55e997c722867b50
Najeeb Uddin <najeebakhun@gmail.com>
2024-03-30 17:35:07 +0500

    Amend js file add object

--- script.js
+++ script.js
@@ -19,7 +19,15 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 const locations = [
   {
     name: "town square",
-    "button text": []
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see a sign that says \"Store\"."
+  },
+  {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store."
   }
 ];
 
