d6c544e3dcbf5a46eab9097a9ae889d0a61f5018
Najeeb Uddin <najeebakhun@gmail.com>
2024-03-30 15:07:25 +0500

    Amend js file add array object

--- script.js
+++ script.js
@@ -16,7 +16,12 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-const locations = [{}];
+const locations = [
+  {
+    name: "town square",
+    "button text": []
+  }
+];
 
 // initialize buttons
 button1.onclick = goStore;
