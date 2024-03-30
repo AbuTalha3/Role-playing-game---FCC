a8ba1f7977ce50549e990ddb52370651a893c21d
Najeeb Uddin <najeebakhun@gmail.com>
2024-03-30 14:50:49 +0500

    Amend js

--- script.js
+++ script.js
@@ -17,10 +17,26 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const locations = [];
+
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function update(location) {
+
+}
+
+function goTown() {
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+}
 
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
@@ -29,6 +45,7 @@ function goStore() {
   button1.onclick = buyHealth;
   button2.onclick = buyWeapon;
   button3.onclick = goTown;
+  text.innerText = "You enter the store.";
 }
 
 function goCave() {
@@ -37,4 +54,12 @@ function goCave() {
 
 function fightDragon() {
   console.log("Fighting dragon.");
+}
+
+function buyHealth() {
+
+}
+
+function buyWeapon() {
+
 }
